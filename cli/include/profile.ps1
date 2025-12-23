a49a1e9f4a537d0f7b8e8bd9174bfb3655c2df7e
Patrick Ambrose (patppuccin) <patrickambrosericky@gmail.com>
2025-12-23 17:38:25 +0530

    chore: cleanup logic

--- cli/include/profile.ps1
+++ cli/include/profile.ps1
@@ -1,10 +1,24 @@
 # ==== PowerShell Profile Configuration ====================
 
+# ==== Check for Minimum PowerShell Version ===============
+
+if ($PSVersionTable.PSVersion.Major -lt 7) {
+    Write-Warning "PowerShell version 7 of higher required for this configuration"
+    Write-Host ""
+    Write-Host " - This profile is located at $($PROFILE)"
+    Write-Host " - PowerShell 7 can be installed via https://aka.ms/install-powershell"
+    Write-Host ""
+    return
+}
+
 # ==== Prepare Environment =================================
 
-$startupLogs = @()
-$loadedTools = @()
-$missingTools = @()
+$StartupLogs = @()
+$LoadedTools = @()
+$MissingTools = @()
+
+$ConfigRoot = if ($env:XDG_CONFIG_HOME) { $env:XDG_CONFIG_HOME } else { Join-Path $HOME '.config' }
+$PwshKitRoot = Join-Path $ConfigRoot 'pwshkit' 'pwshkit.psm1'
 
 $PSStyle.FileInfo.Directory = "" # Disable Directory Highlights (PowerShell versions > 7.3)
 
@@ -16,40 +30,49 @@ $Tools = @(
     [ordered]@{  Name = 'fzf'; Repo = 'https://github.com/junegunn/fzf' },
     [ordered]@{  Name = 'zoxide'; Repo = 'https://github.com/ajeetdsouza/zoxide' },
     [ordered]@{  Name = 'git' ; Repo = 'https://github.com/git/git' },
+    [ordered]@{  Name = 'fastfetch'; Repo = 'https://github.com/fastfetch-cli/fastfetch' },
     [ordered]@{  Name = 'kubectl'; Repo = 'https://github.com/kubernetes/kubernetes' }
 
 ) | ForEach-Object { [PSCustomObject]$_ }
 
 foreach ($tool in $Tools) {
     if (-not (Get-Command $tool.Name -ErrorAction SilentlyContinue)) {
-        $missingTools += $tool
+        $MissingTools += $tool
     }
     else {
-        $loadedTools += $tool
+        $LoadedTools += $tool
     }
 }
 
-$startupLogs += "Failed to load tools: $($missingTools.Name -join ', ')"
+if ($MissingTools.Count -gt 0) {
+    $StartupLogs += "Missing tools: $($MissingTools.Name -join ', ')"
+}
 
-# === Load Utils & Plugins =================================
+# === Load Pwshkit Utils & Plugins =========================
 
-$pwshyRoot = Join-Path $HOME '.pwshy'
-Get-ChildItem (Join-Path $pwshyRoot 'utils') -Filter *.ps1 -Recurse | ForEach-Object { . $_.FullName }
-Get-ChildItem (Join-Path $pwshyRoot 'plugins') -Filter *.ps1 -Recurse | ForEach-Object { . $_.FullName }
+if (Test-Path $PwshKitRoot) {
+    try { Import-Module $PwshKitRoot -ErrorAction Stop }
+    catch { $StartupLogs += "Failed to load pwshkit: $($_.Exception.Message)" }
+}
+else {
+    $StartupLogs += "Pwshkit not found at: $PwshKitRoot"
+}
 
 # ==== Load Tools ==========================================
 
-if ('starship' -in $loadedTools.Name) {
-    $ENV:STARSHIP_CONFIG = "$ENV:USERPROFILE/.config/starship/starship.toml"
-    Invoke-Expression (&starship init powershell)
+if ('starship' -in $LoadedTools.Name) {
+    $ENV:STARSHIP_CONFIG = Join-Path $ConfigRoot 'starship' 'starship.toml'
+    try { Invoke-Expression (&starship init powershell) }
+    catch { $StartupLogs += "Failed to initialize starship" }
 }
 
-if ('fastfetch' -in $loadedTools.Name) {
-    & fastfetch.exe
+if ('fastfetch' -in $LoadedTools.Name) {
+    & fastfetch
 }
 
-if ('zoxide' -in $loadedTools.Name) {
-    Invoke-Expression (& { (zoxide init --cmd cd powershell | Out-String) })
+if ('zoxide' -in $LoadedTools.Name) {
+    try { Invoke-Expression (& { (zoxide init --cmd cd powershell | Out-String) }) } 
+    catch { $StartupLogs += "Failed to initialize zoxide" }
 }
 
 # ==== Load Environment Variables ==========================
---
+++ src/PwshKit.psm1
@@ -0,0 +1,51 @@
+# Module root
+$ModuleRoot = $PSScriptRoot
+
+# Script directories
+$PrivateDirs = @(
+    'helpers'
+)
+
+$PublicDirs = @(
+    'utils',
+    'plugins'
+)
+
+
+# Load private scripts
+foreach ($Dir in $PrivateDirs) {
+    $Path = Join-Path $ModuleRoot $Dir
+    if (-not (Test-Path $Path)) { continue }
+    
+    Get-ChildItem $Path -Filter '*.ps1' -Recurse -File |
+    ForEach-Object {
+        . $_.FullName
+    }
+}
+
+# Load public scripts
+$script:FilesToExport = @()
+
+# Track the files to be exported
+foreach ($dir in $PublicDirs) {
+    $Path = Join-Path $ModuleRoot $dir
+    if (-not (Test-Path $Path)) { continue }
+
+    Get-ChildItem $Path -Filter '*.ps1' -Recurse -File |
+    ForEach-Object {
+        . $_.FullName
+        $script:FilesToExport += $_.FullName
+    }
+}
+
+# Set up Readline configurations
+Set-PSReadlineConfiguration
+
+# Export public functions
+$ExportedFunctions = Get-Command -CommandType Function |
+Where-Object {
+    $_.ScriptBlock.File -and
+    $script:FilesToExport -contains $_.ScriptBlock.File
+}
+
+Export-ModuleMember -Function $ExportedFunctions.Name
